2c56916ce5306241286929927eee6ecc49bbed7e
naldiocj <naldiocj@gmail.com>
2020-10-07 23:34:31 +0100

    Remove import to scroll

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Scroll } from 'react-scroll/modules/mixins/Helpers';
 import SigninForm from '../../components/SigninForm';
 import ScrollToTop from '../../utils/ScrollToTop';
 
